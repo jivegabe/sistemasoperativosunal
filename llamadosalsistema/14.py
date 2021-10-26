b0839c082eed4b12f8f154673232571389ccb628
JohanIvan <jivegabe@unal.edu.co>
2021-10-26 10:52:26 -0500

    Add files via upload

--- llamadosalsistema/14.py
+++ llamadosalsistema/14.py
@@ -1,6 +1,7 @@
 import os
-for root, dirs, files in os.walk(".",True):
+for root, dirs, files in os.walk("C:/Users/johan/Desktop/llamadosalsistema",True):
     print("path:" + root)
     print("directories:" + str(dirs))
     print("files:" + str(files))
     print("########")
+##os.walk recorre todos los directorios y subdirectorios, retornando los paths de los mismos y los nombres de los archivos y directorios que puedan contener
